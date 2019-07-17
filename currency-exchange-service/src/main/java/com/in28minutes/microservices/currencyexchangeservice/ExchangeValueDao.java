76bd34cc23f4c93255951eb38ff6800ce7206411
tomek401273 <tomek371240@gmail.com>
2019-07-17 17:56:47 +0200

    removed-unused-modules

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValueDao.java
+++
@@ -1,9 +0,0 @@
-package com.in28minutes.microservices.currencyexchangeservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExchangeValueDao extends JpaRepository<ExchangeValue, Long> {
-    ExchangeValue findByFromAndTo(String from, String to);
-}
